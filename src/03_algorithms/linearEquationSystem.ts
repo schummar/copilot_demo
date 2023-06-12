bd687c5a50b2a2b109d5b97afe72c7bd25f216e1
Marco Schumacher <marco@schumacher.dev>
2023-06-12 21:03:23 +0000

    .

--- src/03_algorithms/linearEquationSystem.ts
+++ src/03_algorithms/linearEquationSystem.ts
@@ -1,62 +1,61 @@
-
 const A = [
-    [3, 2, -1],
-    [2, -2, 4],
-    [-1, 0.5, -1]
-]
-const b = [1, -2, 0]
-// solution is 
+  [3, 2, -1],
+  [2, -2, 4],
+  [-1, 0.5, -1],
+];
+const b = [1, -2, 0];
+// solution is [1, -2, -2]
 
-// A is a matrix, b a vecor. Solve A*x=b for x
-// minimize numerical errors by pivoting
-function solve(A: number[][], b: number[]): number[] {
-    const n = A.length;
-    const x = new Array(n).fill(0);
+// Solve A*x=b for x
+// minimize numerical error by pivoting
+function solve(A: number[][], b: number[]) {
+  const n = A.length;
+  const x = new Array(n).fill(0);
+  const A_ = A.map((row) => [...row]);
+  const b_ = [...b];
 
-    // forward elimination
-    for (let k = 0; k < n - 1; k++) {
-        // find pivot
-        let iMax = k;
-        let max = Math.abs(A[k][k]);
-        for (let i = k + 1; i < n; i++) {
-            const abs = Math.abs(A[i][k]);
-            if (abs > max) {
-                iMax = i;
-                max = abs;
-            }
-        }
-
-        // swap rows
-        for (let j = k; j < n; j++) {
-            const temp = A[k][j];
-            A[k][j] = A[iMax][j];
-            A[iMax][j] = temp;
-        }
-        const temp = b[k];
-        b[k] = b[iMax];
-        b[iMax] = temp;
-
-        // eliminate
-        for (let i = k + 1; i < n; i++) {
-            const factor = A[i][k] / A[k][k];
-            for (let j = k + 1; j < n; j++) {
-                A[i][j] -= factor * A[k][j];
-            }
-            b[i] -= factor * b[k];
-        }
+  // forward elimination
+  for (let k = 0; k < n - 1; k++) {
+    // find pivot
+    let max = Math.abs(A_[k][k]);
+    let maxRow = k;
+    for (let i = k + 1; i < n; i++) {
+      if (Math.abs(A_[i][k]) > max) {
+        max = Math.abs(A_[i][k]);
+        maxRow = i;
+      }
     }
 
-    // backward substitution
-    x[n - 1] = b[n - 1] / A[n - 1][n - 1];
-    for (let i = n - 2; i >= 0; i--) {
-        let sum = 0;
-        for (let j = i + 1; j < n; j++) {
-            sum += A[i][j] * x[j];
-        }
-        x[i] = (b[i] - sum) / A[i][i];
+    // swap rows
+    for (let i = k; i < n; i++) {
+      const temp = A_[k][i];
+      A_[k][i] = A_[maxRow][i];
+      A_[maxRow][i] = temp;
     }
+    const temp = b_[k];
+    b_[k] = b_[maxRow];
+    b_[maxRow] = temp;
 
-    return x;
+    // elimination
+    for (let i = k + 1; i < n; i++) {
+      const factor = A_[i][k] / A_[k][k];
+      for (let j = k + 1; j < n; j++) {
+        A_[i][j] -= factor * A_[k][j];
+      }
+      b_[i] -= factor * b_[k];
+    }
+  }
+
+  // back substitution
+  for (let i = n - 1; i >= 0; i--) {
+    let sum = 0;
+    for (let j = i + 1; j < n; j++) {
+      sum += A_[i][j] * x[j];
+    }
+    x[i] = (b_[i] - sum) / A_[i][i];
+  }
+
+  return x;
 }
 
 console.log(solve(A, b));
